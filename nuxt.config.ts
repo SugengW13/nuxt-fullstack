9aac1a360003636325e72180dda0e7864affc3ba
sugengw <sugeng.warsito@transtrack.id>
2024-08-13 13:42:02 +0700

    feat: prisma

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   devServer: { port: 3030 },
-  modules: [
-    '@nuxt/ui',
-    '@nuxtjs/supabase'
-  ]
+  modules: ['@nuxt/ui', '@nuxtjs/supabase', "@prisma/nuxt"],
+  supabase: {
+    url: process.env.DATABASE_URL,
+    key: process.env.DATABASE_KEY
+  }
 })
