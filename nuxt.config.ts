6f2a9f6f525f11cb314d29f9ea268a10af89ae21
sugengw <sugeng.warsito@transtrack.id>
2024-08-13 14:50:42 +0700

    feat: sidebase aut

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   devServer: { port: 3030 },
-  modules: ['@nuxt/ui', '@nuxtjs/supabase', "@prisma/nuxt"],
+  modules: [
+    '@nuxt/ui',
+    '@nuxtjs/supabase',
+    '@prisma/nuxt',
+    '@sidebase/nuxt-auth'
+  ],
   supabase: {
     url: process.env.DATABASE_URL,
     key: process.env.DATABASE_KEY
