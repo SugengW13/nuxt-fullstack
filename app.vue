a182bb8ebfb950bb32db8c2106bd7ca90a663e3e
sugengw <sugeng.warsito@transtrack.id>
2024-08-13 11:43:10 +0700

    feat: nuxtui

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+onMounted(() => {
+  colorMode.preference = 'light'
+})
+</script>
+
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <h1 class="text-primary-500 dark:text-white">Test</h1>
+  </div>
+</template>
