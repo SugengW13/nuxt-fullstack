c8b1b41209d04c03a75b9488e7329416da3660d2
sugengw <sugeng.warsito@transtrack.id>
2024-08-13 13:11:31 +0700

    feat: supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  devServer: {port: 3030},
-  modules: ["@nuxt/ui"]
+  devServer: { port: 3030 },
+  modules: [
+    '@nuxt/ui',
+    '@nuxtjs/supabase'
+  ]
 })
